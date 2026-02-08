834250362b807c5cd512b0186573d98407e6c288
William Dalian <williamdalian1@gmail.com>
2026-02-07 19:46:42 -0500

    Update backend

--- backend/AITools.py
+++ backend/AITools.py
@@ -1,12 +1,12 @@
 from dotenv import load_dotenv
-import os
 
-import requests
-import json
 
 from datetime import datetime
 
+import requests
 
+import json
+import os
 
 def openRouterCall(message, systemPrompt):
     load_dotenv()
@@ -15,7 +15,6 @@ def openRouterCall(message, systemPrompt):
 
     #get todays date to handle vauge dates, since gemini doesnt know what day is??
     now = datetime.now()
-
 
 # First API call with reasoning
     response = requests.post(
@@ -65,30 +64,67 @@ def openRouterCall(message, systemPrompt):
     )
     '''
     print(response)
-    return response, reasoning
+    return response
+
+def getParticipantsDiscord(query):
+    now = datetime.now()
+    systemPrompt = """You are a secretary responsible for scheduing meetings. Please return a list of all intended participants, 
+    and addiitonal scheduling instructions.
+    You are to return only valid json in the following format: 
+    {
+        "usernames": [{"discord_name_1": "<participant's discord name>"}, ...],
+        "instructions": "<Additional instructions for scheduling. If a specific date is not given, do not add a specific date>" 
+    }
+    """
+
+    response = openRouterCall(query, systemPrompt)
+    print(response)
+
+    return response['content']
+
+
+def getTimes(query, userCalendars):
+    systemPrompt = """
+        You are a secretary responsible for scheduing meetings. Please return only valid json in the following
+        format about the details of meeting times. When you are unable to schedule all participants, please still return possible options,
+        and who is able to attend.:
+     {
+    "action": "requested_action",
+    "title": "meeting_title",
+    "duration_minutes": 30,
+    "potential_meeting_dates_and_times": [
+        {
+        "start_date": "YYYY-MM-DD",
+        "start_time": "HH:MM",
+        "end_date": "YYYY-MM-DD",
+        "end_time": "HH:MM",
+        "participants_available": [
+            "<name OR @discord_username>",
+            "<name OR @discord_username>"
+        ]
+        },
+        {
+        "start_date": "YYYY-MM-DD",
+        "start_time": "HH:MM",
+        "end_date": "YYYY-MM-DD",
+        "end_time": "HH:MM",
+        "participants_available": [
+            "<name OR @discord_username>",
+            "<name OR @discord_username>"
+        ]
+        }
+    ]
+    }
+    """
+
+    meetingTimesJson = openRouterCall(query, systemPrompt)
 
 def main():
-    load_dotenv()
-    openRouterKey = os.getenv('OR_API_KEY')
+    samplePrompt = f"Lets schedule a meeting for a sunday afternoon between @jimBob,  @BoPeep, @conor, and @super. @super and @jimbob should be able to attend."
 
-    systemPrompt = """
-        You are a secretary responsible for scheduing meetings. You are not to create a final meeting time. Only return potential meeting times, and include several, if possible.
-        Please return only valid json in the following
-        format about the details of the natural language provided using ISO standard date and time format:
-        {
-        "requested_action": "schedule_meeting",
-        "title": "<meeting_title>",
-        "duration_minutes": (int) meeting duration in minutes,
-        "participants": [please list meeting participants],
-        "potential_dates_and_times": ["block1":{"start_date": <start_date>, "start_time": "start_time", "end_date": "end_date", "end_time": "end_time"},
-             "block2": {"start_date": "start_date", "start_time": "start_time", "end_date": "end_date", "end_time": "end_time"},... ],
-        }
-        """
-
-    openRouterCall("Lets schedule a meeting with tom and joe.", systemPrompt)
-
-
+    discordUsers = getParticipantsDiscord(samplePrompt)
+    print(discordUsers)
 
 if __name__ == "__main__":
-    print("calling main")
     main()
+
